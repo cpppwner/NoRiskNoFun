25543dfc5c61baaaff3f89e763b1f120ebd54af4
juurbani <juurbani@edu.aau.at>
2017-04-05 16:59:03 +0200

    GUI Assets

--- core/src/gmbh/norisknofun/NoRiskNoFun.java
+++ core/src/gmbh/norisknofun/NoRiskNoFun.java
@@ -5,16 +5,37 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class NoRiskNoFun extends ApplicationAdapter {
 	Stage stage;
 	Texture img;
+	Texture imgbtn;
+	TextureRegion imgbtnregion;
+	TextureRegionDrawable textureRegionDrawable;
+	ImageButton imageButton;
 
 	@Override
 	public void create () {
 		stage = new Stage();
 		img = new Texture("menu.png");
+		imgbtn = new Texture(Gdx.files.internal("button_create_game_eng.png"));
+		imgbtnregion= new TextureRegion(imgbtn);
+		textureRegionDrawable = new TextureRegionDrawable(imgbtnregion);
+		imageButton= new ImageButton(textureRegionDrawable);
+		imageButton.setBounds(300,300,553,480);
+		stage.addActor(imageButton);
+
+		imgbtn = new Texture(Gdx.files.internal("button_join_game_eng.png"));
+		imgbtnregion= new TextureRegion(imgbtn);
+		textureRegionDrawable = new TextureRegionDrawable(imgbtnregion);
+		imageButton= new ImageButton(textureRegionDrawable);
+		imageButton.setBounds(900,300,553,480);
+		stage.addActor(imageButton);
+
 	}
 	@Override
 	public void render () {
