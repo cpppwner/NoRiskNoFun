a2b98b4bfcd7f098f1ab13e57d8e11b044043753
juurbani <juurbani@edu.aau.at>
2017-04-05 16:47:18 +0200

    GUI Assets

--- core/src/gmbh/norisknofun/NoRiskNoFun.java
+++ core/src/gmbh/norisknofun/NoRiskNoFun.java
@@ -5,29 +5,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class NoRiskNoFun extends ApplicationAdapter {
-	SpriteBatch batch;
+	Stage stage;
 	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
 	@Override
+	public void create () {
+		stage = new Stage();
+		img = new Texture("menu.png");
+	}
+	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.getBatch().begin();
+		stage.getBatch().draw(img,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		stage.getBatch().end();
+		stage.draw();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		stage.dispose();
 		img.dispose();
 	}
+
+
+
 }
