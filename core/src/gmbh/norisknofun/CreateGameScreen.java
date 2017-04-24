30eeeab0776b505067a4a2cf7f8915d0e329bc59
juurbani <juurbani@edu.aau.at>
2017-04-24 13:50:09 +0200

    GUI Assets

---
+++ core/src/gmbh/norisknofun/CreateGameScreen.java
@@ -0,0 +1,70 @@
+package gmbh.norisknofun;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+/**
+ * Created by Sputzi0815 on 24.04.2017.
+ */
+
+public class CreateGameScreen extends Game implements Screen {
+    Stage createGame;
+    Texture img;
+    Texture imgbtn;
+    TextureRegion imgbtnregion;
+    TextureRegionDrawable textureRegionDrawable;
+    ImageButton imageButton;
+
+    public CreateGameScreen(NoRiskNoFun game) {
+
+    }
+
+
+    @Override
+    public void create(){
+        createGame = new Stage();
+        img = new Texture("menu.png");
+
+    }
+
+    @Override
+    public void render(){
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        createGame.getBatch().begin();
+        createGame.getBatch().draw(img,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        createGame.getBatch().end();
+        createGame.draw();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose(){
+        createGame.dispose();
+        img.dispose();
+    }
+
+}
--- core/src/gmbh/norisknofun/NoRiskNoFun.java
+++ core/src/gmbh/norisknofun/NoRiskNoFun.java
@@ -1,56 +1,105 @@
 package gmbh.norisknofun;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
-public class NoRiskNoFun extends ApplicationAdapter {
-	Stage stage;
+public class NoRiskNoFun extends Game implements Screen{
+	Stage menu;
 	Texture img;
 	Texture imgbtn;
 	TextureRegion imgbtnregion;
 	TextureRegionDrawable textureRegionDrawable;
-	ImageButton imageButton;
+	ImageButton imageButtonCreate;
+	ImageButton imageButtonJoin;
 
-	@Override
-	public void create () {
-		stage = new Stage();
+	private final NoRiskNoFun game;
+
+
+	public NoRiskNoFun(){
+		game = this;
+		menu = new Stage();
+		Gdx.input.setInputProcessor(menu);
+
 		img = new Texture("menu.png");
 		imgbtn = new Texture(Gdx.files.internal("button_create_game_eng.png"));
 		imgbtnregion= new TextureRegion(imgbtn);
 		textureRegionDrawable = new TextureRegionDrawable(imgbtnregion);
-		imageButton= new ImageButton(textureRegionDrawable);
-		imageButton.setBounds(300,300,553,480);
-		stage.addActor(imageButton);
+		imageButtonCreate= new ImageButton(textureRegionDrawable);
+		imageButtonCreate.setBounds((Gdx.graphics.getWidth()/6)-5,(Gdx.graphics.getHeight()/3),553,480);
+		menu.addActor(imageButtonCreate);
+
+		imageButtonCreate.addListener( new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				setScreen(new CreateGameScreen(game));
+			}
+		});
+
+
 
 		imgbtn = new Texture(Gdx.files.internal("button_join_game_eng.png"));
 		imgbtnregion= new TextureRegion(imgbtn);
 		textureRegionDrawable = new TextureRegionDrawable(imgbtnregion);
-		imageButton= new ImageButton(textureRegionDrawable);
-		imageButton.setBounds(900,300,553,480);
-		stage.addActor(imageButton);
+		imageButtonJoin= new ImageButton(textureRegionDrawable);
+		imageButtonJoin.setBounds((Gdx.graphics.getWidth()/2)+10,(Gdx.graphics.getHeight()/3),553,480);
+		menu.addActor(imageButtonJoin);
+
+		imageButtonJoin.addListener( new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				setScreen(new JoinGameScreen(game));
+			}
+		});
+
+
 
 	}
+
+
 	@Override
-	public void render () {
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.getBatch().begin();
-		stage.getBatch().draw(img,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		stage.getBatch().end();
-		stage.draw();
+		menu.getBatch().begin();
+		menu.getBatch().draw(img,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		menu.getBatch().end();
+		menu.draw();
+	}
+
+	@Override
+	public void create() {
 
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
 	@Override
 	public void dispose () {
-		stage.dispose();
+		menu.dispose();
 		img.dispose();
 	}
 
