6cb3a65c5f2930b7ec5ccb7a2edbf58ad08a9b6b
philipp1996 <pmoedritscher@yahoo.com>
2017-04-26 14:31:12 +0200

    Merge branches 'DiceRollTest' and 'master' of https://github.com/cpppwner/NoRiskNoFun
    # Conflicts:
    #	core/src/gmbh/norisknofun/NoRiskNoFun.java

--- core/src/gmbh/norisknofun/NoRiskNoFun.java
+++ core/src/gmbh/norisknofun/NoRiskNoFun.java
@@ -8,7 +8,9 @@ import gmbh.norisknofun.game.GameData;
 import gmbh.norisknofun.scene.Scene;
 import gmbh.norisknofun.scene.SceneManager;
 import gmbh.norisknofun.scene.SceneNames;
+import gmbh.norisknofun.scene.ui.CreateGameScene;
 import gmbh.norisknofun.scene.game.GameScene;
+import gmbh.norisknofun.scene.ui.JoinGameScene;
 import gmbh.norisknofun.scene.ui.MapSelectionScene;
 
 public class NoRiskNoFun implements ApplicationListener  {
@@ -25,11 +27,14 @@ public class NoRiskNoFun implements ApplicationListener  {
         GameData gameData = new GameData(new AssetLoaderFactoryImpl());
         SceneManager.getInstance().registerScene(new MapSelectionScene(gameData));
         SceneManager.getInstance().registerScene(new GameScene(gameData));
+        SceneManager.getInstance().registerScene(new CreateGameScene(gameData));
+        SceneManager.getInstance().registerScene(new JoinGameScene(gameData));
     }
 
     private void showStartupScene() {
 
-        SceneManager.getInstance().setActiveScene(SceneNames.MAP_SELECTION_SCENE);
+        SceneManager.getInstance().setActiveScene(SceneNames.MAP_SELECTION_SCENE
+        );
     }
 
     @Override
