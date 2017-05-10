4f955203f34de430c865e3159c80e9e2d36bbab1
philipp1996 <pmoedritscher@yahoo.com>
2017-05-10 14:25:53 +0200

    Merge branch 'master' of https://github.com/cpppwner/NoRiskNoFun into NetworkMessages
    # Conflicts:
    #	android/src/gmbh/norisknofun/MainActivity.java
    #	core/src/gmbh/norisknofun/Network/Client.java
    #	core/src/gmbh/norisknofun/Network/MyServer.java
    #	core/src/gmbh/norisknofun/Network/ServerDispatcher.java

--- android/src/gmbh/norisknofun/CheckConnection.java
+++ android/src/gmbh/norisknofun/CheckConnection.java
@@ -2,7 +2,7 @@ package gmbh.norisknofun;
 
 
 /**
- * Created by Philipp Mödritscher on 02.05.2017.
+ * Created by Philipp Mödritscher on 02.05.2017.+
  */
 
 public class CheckConnection extends Thread{
