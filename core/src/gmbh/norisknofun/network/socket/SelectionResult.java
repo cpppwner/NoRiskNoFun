cabf50fb532cf82279dd2dc5159a15c8edbaec44
Stefan Eberl <cpppwner@gmail.com>
2017-05-15 01:52:35 +0200

    Add JavaDoc to SelectionResult interface

--- core/src/gmbh/norisknofun/network/socket/SelectionResult.java
+++ core/src/gmbh/norisknofun/network/socket/SelectionResult.java
@@ -1,22 +1,67 @@
 package gmbh.norisknofun.network.socket;
 
+import java.nio.ByteBuffer;
 import java.util.Set;
 
 /**
- * Created by cpppwner on 11.05.17.
+ * Interface specifying the result of a {@link SocketSelector#select()} operation..
  */
-
 public interface SelectionResult {
 
+    /**
+     * Get all {@link TCPServerSocket server sockets} that are in accepting state.
+     *
+     * <p>
+     *     After performing the {@link TCPServerSocket#accept()} operation, call the
+     *     {@link SelectionResult#acceptHandled(TCPServerSocket)} method.
+     * </p>
+     */
     Set<TCPServerSocket> getAcceptableSockets();
 
+    /**
+     * Method that shall be called, when an accept was handled.
+     *
+     * <p>
+     *     This method must be called, in order to remove this pending event.
+     * </p>
+     */
     void acceptHandled(TCPServerSocket socket);
 
+    /**
+     * Get all {@link TCPClientSocket client sockets} that can be read from.
+     *
+     * <p>
+     *     After performing the {@link TCPClientSocket#read(ByteBuffer)} ()} operation, call the
+     *     {@link SelectionResult#readHandled(TCPClientSocket)} method.
+     * </p>
+     */
     Set<TCPClientSocket> getReadableSockets();
 
+    /**
+     * Method that shall be called, when a read operation was handled.
+     *
+     * <p>
+     *     This method must be called, in order to remove this pending event.
+     * </p>
+     */
     void readHandled(TCPClientSocket socket);
 
+    /**
+     * Get all {@link TCPClientSocket client sockets} that can be written to.
+     *
+     * <p>
+     *     After performing the {@link TCPClientSocket#write(ByteBuffer)} ()} operation, call the
+     *     {@link SelectionResult#writeHandled(TCPClientSocket)} method.
+     * </p>
+     */
     Set<TCPClientSocket> getWritableSockets();
 
+    /**
+     * Method that shall be called, when a write operation was handled.
+     *
+     * <p>
+     *     This method must be called, in order to remove this pending event.
+     * </p>
+     */
     void writeHandled(TCPClientSocket socket);
 }
