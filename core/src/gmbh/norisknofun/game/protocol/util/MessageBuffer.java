5dae3ac2ff33639d37bd9f094809c0c23eb0e2bc
Stefan Eberl <cpppwner@gmail.com>
2017-06-02 16:21:22 +0200

    Add implemenation for MessageBuffer

--- core/src/gmbh/norisknofun/game/protocol/util/MessageBuffer.java
+++ core/src/gmbh/norisknofun/game/protocol/util/MessageBuffer.java
@@ -1,8 +1,95 @@
 package gmbh.norisknofun.game.protocol.util;
 
 /**
- * Created by cpppwner on 01.06.17.
+ * Utility class wrapping a byte buffer, which is can grow and shrink.
  */
+public class MessageBuffer {
 
-public class ByteBuffer {
+    static final int INITIAL_BUFFER_LENGTH = 4096;
+
+    private byte[] buffer;
+    private int readOffset;
+    private int writeOffset;
+
+    public MessageBuffer() {
+        buffer = new byte[INITIAL_BUFFER_LENGTH];
+        readOffset = 0;
+        writeOffset = 0;
+    }
+
+    public int length() {
+
+        return writeOffset - readOffset;
+    }
+
+    public int capacity() {
+
+        return buffer.length;
+    }
+
+    public void append(byte[] data) {
+
+        ensureWriteCapacity(data.length);
+        appendToBuffer(data);
+    }
+
+    private void ensureWriteCapacity(int length) {
+
+        int remainingWriteCapacity = buffer.length - writeOffset;
+        if (remainingWriteCapacity < length) {
+            // write capacity is too low - reallocation necessary
+            int additionalSizeRequired = length - (buffer.length - writeOffset);
+            int newCapacity = buffer.length + (((additionalSizeRequired / INITIAL_BUFFER_LENGTH) + 1) * INITIAL_BUFFER_LENGTH);
+            byte[] tmp = new byte[newCapacity];
+            System.arraycopy(buffer, 0, tmp, 0, writeOffset);
+            buffer = tmp;
+        }
+    }
+
+    private void appendToBuffer(byte[] data) {
+
+        System.arraycopy(data, 0, buffer, writeOffset, data.length);
+        writeOffset += data.length;
+    }
+
+    public byte[] read(int length) {
+
+        byte[] result = peek(length);
+        readOffset += length;
+        compactBuffer();
+
+        return result;
+    }
+
+    public byte[] peek(int length) {
+
+        if (length < 0) {
+            throw new IllegalArgumentException("length is less than zero");
+        }
+
+
+        if (length > length()) {
+            throw new IllegalArgumentException("length is greater than length()");
+        }
+
+        byte[] result = new byte[length];
+        System.arraycopy(buffer, readOffset, result, 0, result.length);
+
+        return result;
+    }
+
+    private void compactBuffer() {
+
+        if (readOffset >= INITIAL_BUFFER_LENGTH) {
+            System.arraycopy(buffer, readOffset, buffer, 0, length());
+            writeOffset -= readOffset;
+            readOffset = 0;
+        }
+    }
+
+    public void clear() {
+
+        readOffset = 0;
+        writeOffset = 0;
+    }
 }
--- core/test/gmbh/norisknofun/game/protocol/util/MessageBufferTests.java
+++ core/test/gmbh/norisknofun/game/protocol/util/MessageBufferTests.java
@@ -1,8 +1,311 @@
 package gmbh.norisknofun.game.protocol.util;
 
-/**
- * Created by cpppwner on 02.06.17.
- */
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+import java.util.Random;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for testing {@link MessageBuffer}.
+ */
 public class MessageBufferTests {
+
+    private static byte[] bigData;
+    private static byte[] smallData;
+
+    @BeforeClass
+    public static void setupFixture() {
+
+        bigData = new byte[4 * MessageBuffer.INITIAL_BUFFER_LENGTH + MessageBuffer.INITIAL_BUFFER_LENGTH / 4];
+        new Random().nextBytes(bigData);
+
+        smallData = new byte[MessageBuffer.INITIAL_BUFFER_LENGTH / 4];
+        new Random().nextBytes(smallData);
+    }
+
+    @Test
+    public void aDefaultConstructedMessageBufferHasInitialCapacity() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        int obtained = target.capacity();
+
+        // then
+        assertThat(obtained, is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+    }
+
+    @Test
+    public void aDefaultConstructedMessageBufferHasZeroLength() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        int obtained = target.length();
+
+        // then
+        assertThat(obtained, is(0));
+    }
+
+    @Test
+    public void appendDataAdjustsLengthAccordingly() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        target.append(smallData);
+
+        // then
+        assertThat(target.length(), is(smallData.length));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+    }
+
+    @Test
+    public void appendDataInSmallerChunksAdjustsLengthAccordingly() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        for (int i = 0; i < smallData.length; i += 10) {
+            byte[] data = new byte[Math.min(10, smallData.length - i)];
+            System.arraycopy(smallData, i, data, 0, data.length);
+            target.append(data);
+        }
+
+        // then
+        assertThat(target.length(), is(smallData.length));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+    }
+
+    @Test
+    public void appendDataAdjustsCapacityAccordingly() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        target.append(bigData);
+
+        // then
+        assertThat(target.length(), is(bigData.length));
+        assertThat(target.capacity(), is(greaterThanOrEqualTo(bigData.length)));
+        assertThat(target.capacity() % MessageBuffer.INITIAL_BUFFER_LENGTH, is(0));
+    }
+
+    @Test
+    public void appendDataInSmallerChunksAdjustsCapacityAccordingly() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        for (int i = 0; i < bigData.length; i += 10) {
+            byte[] data = new byte[Math.min(10, bigData.length - i)];
+            System.arraycopy(bigData, i, data, 0, data.length);
+            target.append(data);
+        }
+
+        // then
+        assertThat(target.length(), is(bigData.length));
+        assertThat(target.capacity(), is(greaterThanOrEqualTo(bigData.length)));
+        assertThat(target.capacity() % MessageBuffer.INITIAL_BUFFER_LENGTH, is(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void readingLessThanZeroBytesThrowsException() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when/then
+        target.read(-1);
+    }
+
+    @Test
+    public void readingZeroBytesFromEmptyBufferWorks() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        byte[] obtained = target.read(0);
+
+        // then
+        assertThat(obtained.length, is(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void readingFromAnEmptyBufferThrowsException() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when/then
+        target.read(1);
+    }
+
+    @Test
+    public void readingFromFilledGivesReadData() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+        target.append(smallData);
+
+        // when reading 0 bytes from filled buffer
+        byte[] obtained = target.read(0);
+
+        // then
+        assertThat(obtained.length, is(0));
+        assertThat(target.length(), is(smallData.length));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+
+        // and when reading some data from filled buffer
+        obtained = target.read(smallData.length / 2);
+
+        // then
+        assertThat(obtained.length, is(smallData.length / 2));
+        assertThat(target.length(), is(smallData.length - (smallData.length / 2)));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+
+        byte[] expectedOutput = new byte[smallData.length / 2];
+        System.arraycopy(smallData, 0, expectedOutput, 0, expectedOutput.length);
+        assertThat(obtained, is(equalTo(expectedOutput)));
+
+        // and when consuming remaining data
+        obtained = target.read(target.length());
+
+        // then
+        assertThat(obtained.length, is(smallData.length - (smallData.length / 2)));
+        assertThat(target.length(), is(0));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+
+        expectedOutput = new byte[smallData.length - (smallData.length / 2)];
+        System.arraycopy(smallData, smallData.length / 2, expectedOutput, 0, expectedOutput.length);
+        assertThat(obtained, is(equalTo(expectedOutput)));
+    }
+
+    @Test
+    public void readingDataFromBufferDoesNotDecreaseCapacity() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+        target.append(bigData);
+        int capacity = target.capacity();
+
+        // and when reading some data from filled buffer
+        byte[] obtained = target.read(bigData.length / 2);
+
+        // then
+        assertThat(obtained.length, is(bigData.length / 2));
+        assertThat(target.length(), is(bigData.length - (bigData.length / 2)));
+        assertThat(target.capacity(), is(capacity));
+
+        byte[] expectedOutput = new byte[bigData.length / 2];
+        System.arraycopy(bigData, 0, expectedOutput, 0, expectedOutput.length);
+        assertThat(obtained, is(equalTo(expectedOutput)));
+
+        // and when consuming remaining data
+        obtained = target.read(target.length());
+
+        // then
+        assertThat(obtained.length, is(bigData.length - (bigData.length / 2)));
+        assertThat(target.length(), is(0));
+        assertThat(target.capacity(), is(capacity));
+
+        expectedOutput = new byte[bigData.length - (bigData.length / 2)];
+        System.arraycopy(bigData, bigData.length / 2, expectedOutput, 0, expectedOutput.length);
+        assertThat(obtained, is(equalTo(expectedOutput)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void peekingLessThanZeroBytesThrowsException() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when/then
+        target.peek(-1);
+    }
+
+    @Test
+    public void peekingZeroBytesFromEmptyBufferWorks() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when
+        byte[] obtained = target.peek(0);
+
+        // then
+        assertThat(obtained.length, is(0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void peekingFromAnEmptyBufferThrowsException() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+
+        // when/then
+        target.peek(1);
+    }
+
+    @Test
+    public void peekingFromFilledGivesReadData() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+        target.append(smallData);
+
+        // when reading 0 bytes from filled buffer
+        byte[] obtained = target.peek(0);
+
+        // then
+        assertThat(obtained.length, is(0));
+        assertThat(target.length(), is(smallData.length));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+
+        // and when peeking all data
+        obtained = target.peek(smallData.length);
+
+        // then
+        assertThat(obtained.length, is(smallData.length));
+        assertThat(target.length(), is(smallData.length));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+        assertThat(obtained, is(equalTo(smallData)));
+        assertThat(obtained, is(not(sameInstance(smallData))));
+
+        // and when peeking again
+        obtained = target.peek(smallData.length);
+
+        // then
+        assertThat(obtained.length, is(smallData.length));
+        assertThat(target.length(), is(smallData.length));
+        assertThat(target.capacity(), is(MessageBuffer.INITIAL_BUFFER_LENGTH));
+        assertThat(obtained, is(equalTo(smallData)));
+        assertThat(obtained, is(not(sameInstance(smallData))));
+    }
+
+    @Test
+    public void clearingBufferOnlyChangesLength() {
+
+        // given
+        MessageBuffer target = new MessageBuffer();
+        target.append(bigData);
+        int capacity = target.capacity();
+
+        // when
+        target.clear();
+
+        // then
+        assertThat(target.length(), is(0));
+        assertThat(target.capacity(), is(capacity));
+    }
 }
