faf36441be813c2f7478d351ff09465f85850307
juurbani <juurbani@edu.aau.at>
2017-06-06 18:36:08 +0200

    Accidently removed "Texts.java" with an unwanted Pull Re-Adding "Texts.java"

---
+++ core/src/gmbh/norisknofun/scene/Texts.java
@@ -0,0 +1,11 @@
+package gmbh.norisknofun.scene;
+
+/**
+ * Class containing constant texts displayed in the application.
+ */
+public final class Texts {
+
+    public static final String APPLICATION_TITLE = "No Risk No Fun!";
+
+    private Texts() {}
+}
